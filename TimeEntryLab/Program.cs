7b5953067f42828cea0f7764943d3074c1906235
Tee Link <tesuanseylink@gmail.com>
2016-03-17 14:49:08 -0500

    Initial Commit

---
+++ TimeEntryLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeEntryLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new TimeEntryDBContext();
+
+            var developers = db.Developers.ToList();
+        }
+    }
+}
